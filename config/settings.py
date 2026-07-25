7d76ec80b9a11235d57684e36c3ce6917c51ac1c
newmorin2 <newton.mwangi@student.moringaschool.com>
2026-07-25 18:48:29 +0300

    configuring render

--- config/settings.py
+++ config/settings.py
@@ -26,7 +26,9 @@ SECRET_KEY = 'django-insecure-jz^_dma+z6mxx#9t0-o)_nrs7hqb91coo==kggz1%$1h@#s(wx
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["your-render-domain.onrender.com",
+    "localhost",
+    "127.0.0.1"]
 
 
 # Application definition
@@ -120,6 +122,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
